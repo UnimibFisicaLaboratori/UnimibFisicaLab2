f9f63c3fc5e198f7035a7e3a85cf6ba1470284c9
govoni <pietro.govoni@gmail.com>
2021-11-09 10:10:52 +0100

    aggiunta chiusa ed esempio di TGraph

--- Lezione_07/Esercizi/esercizio05.cpp
+++ Lezione_07/Esercizi/esercizio05.cpp
@@ -4,6 +4,7 @@ c++ -o esercizio05 `root-config --glibs --cflags` esercizio05.cpp casual.cc
 
 #include "casual.h"
 #include "statistiche.h"
+#include "TH1F.h"
 #include "TGraph.h"
 #include "TCanvas.h"
 #include <iostream>
@@ -19,7 +20,7 @@ int main (int argc, char ** argv)
   TGraph g_curtosi ;
 
   // ciclo sulla media della distribuzione di Poisson
-  for (int mediaP = 1 ; mediaP < 1001 ; mediaP+=10)
+  for (int mediaP = 1 ; mediaP < 101 ; mediaP+=5)
     {
       cout << "generazioni con media " << mediaP << endl ;
       vector<int> campione ;
@@ -37,24 +38,41 @@ int main (int argc, char ** argv)
 
     } // ciclo sulla media della distribuzione di Poisson
 
-  TCanvas c1 ;
-  g_media.SetMarkerStyle (4) ;
-  g_media.SetMarkerColor (kBlue) ;
+  TCanvas c1 ("c1", "c1", 100, 100, 1000, 1000) ;
+
+  g_media.SetMarkerStyle (20) ;
+  g_media.SetMarkerColor (kAzure - 1) ;
+  g_media.SetLineColor (kGray + 1) ;
+  g_media.SetMarkerSize (2) ;
+  g_media.GetHistogram ()->GetXaxis ()->SetTitle ("media della distribuzione") ;
+  g_media.GetHistogram ()->GetYaxis ()->SetTitle ("media del campione") ;
   g_media.Draw ("ALP") ;
   c1.Print ("es5_media.gif", "gif") ;
 
-  g_sigma.SetMarkerStyle (4) ;
-  g_sigma.SetMarkerColor (kBlue) ;
+  g_sigma.SetMarkerStyle (20) ;
+  g_sigma.SetMarkerColor (kAzure - 1) ;
+  g_sigma.SetLineColor (kGray + 1) ;
+  g_sigma.SetMarkerSize (2) ;
+  g_sigma.GetHistogram ()->GetXaxis ()->SetTitle ("media della distribuzione") ;
+  g_sigma.GetHistogram ()->GetYaxis ()->SetTitle ("deviazione standard del campione") ;
   g_sigma.Draw ("ALP") ;
   c1.Print ("es5_sigma.gif", "gif") ;
 
-  g_asimmetria.SetMarkerStyle (4) ;
-  g_asimmetria.SetMarkerColor (kBlue) ;
+  g_asimmetria.SetMarkerStyle (20) ;
+  g_asimmetria.SetMarkerColor (kAzure - 1) ;
+  g_asimmetria.SetLineColor (kGray + 1) ;
+  g_asimmetria.SetMarkerSize (2) ;
+  g_asimmetria.GetHistogram ()->GetXaxis ()->SetTitle ("media della distribuzione") ;
+  g_asimmetria.GetHistogram ()->GetYaxis ()->SetTitle ("asimmetria del campione") ;
   g_asimmetria.Draw ("ALP") ;
   c1.Print ("es5_asimmetria.gif", "gif") ;
 
-  g_curtosi.SetMarkerStyle (4) ;
-  g_curtosi.SetMarkerColor (kBlue) ;
+  g_curtosi.SetMarkerStyle (20) ;
+  g_curtosi.SetMarkerColor (kAzure - 1) ;
+  g_curtosi.SetLineColor (kGray + 1) ;
+  g_curtosi.SetMarkerSize (2) ;
+  g_curtosi.GetHistogram ()->GetXaxis ()->SetTitle ("media della distribuzione") ;
+  g_curtosi.GetHistogram ()->GetYaxis ()->SetTitle ("curtosi del campione") ;
   g_curtosi.Draw ("ALP") ;
   c1.Print ("es5_curtosi.gif", "gif") ;
 
